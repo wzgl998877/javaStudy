f04fcde17031050c0d817be1afa347a085524a45
zt <2458425315@qq.com>
2020-12-19 17:54:23 +0800

    feat() 写ppt好烦，不如刷个简单的题

--- src/main/java/com/zt/javastudy/JavaStudyApplication.java
+++ src/main/java/com/zt/javastudy/JavaStudyApplication.java
@@ -2,8 +2,9 @@ package com.zt.javastudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class JavaStudyApplication {
 
     public static void main(String[] args) {
