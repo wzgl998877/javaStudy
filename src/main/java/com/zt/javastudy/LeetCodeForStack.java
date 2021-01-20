b452a5d60cd02cf93d8d7efeed8f4a2b4def6150
zt <2458425315@qq.com>
2021-01-20 21:04:57 +0800

    feat() 脑残的自己还是要算法啊，贪心递归这些，前面刷的题都是没算法的

--- src/main/java/com/zt/javastudy/LeetCodeForStack.java
+++ src/main/java/com/zt/javastudy/LeetCodeForStack.java
@@ -2,7 +2,6 @@ package com.zt.javastudy;
 
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Stack;
 
 /**
  * @author zhengtao
@@ -12,8 +11,15 @@ import java.util.Stack;
 public class LeetCodeForStack {
     public static void main(String[] args) {
         String path = "/home//foo/";
-        System.out.println(simplifyPath1(path));
+//        System.out.println(simplifyPath1(path));
+        System.out.println(removeKdigits("1111", 1));
     }
+
+    /**
+     * 71. 简化路径
+     * @param path
+     * @return
+     */
     public static String simplifyPath(String path) {
         String[] strings = path.split("/");
         Deque<String> stack = new LinkedList<>();
@@ -58,5 +64,57 @@ public class LeetCodeForStack {
             res = "/" + d + res;
         }
         return res.isEmpty() ? "/" : res;
+    }
+
+    /**
+     * 402. 移掉K位数字 跟个傻逼一样这题，自己在干嘛啊
+     * 最好的就是贪心啊
+     * @param num
+     * @param k
+     * @return
+     */
+    public static String removeKdigits(String num, int k) {
+        if (num.length()==k) {
+            return "0";
+        }
+        char[] numArray = num.toCharArray();
+        Deque<Character> stack = new LinkedList<>();
+        stack.push(numArray[0]);
+        for (int i=1;i<numArray.length;i++){
+            if (k>0){
+                if (!stack.isEmpty()) {
+                    if ((int) stack.peek() > (int) numArray[i]) {
+                        stack.pop();
+                        stack.push(numArray[i]);
+                        k--;
+                    } else if ((int) stack.peek() == (int) numArray[i]) {
+                        stack.push(numArray[i]);
+                    } else {
+                        k--;
+                    }
+                } else {
+                    stack.push(numArray[i]);
+                }
+            }else {
+                stack.push(numArray[i]);
+            }
+        }
+        if (stack.isEmpty()) return "0";
+        Deque<Character> stack1 = new LinkedList<>();
+        while (!stack.isEmpty()){
+            stack1.push(stack.pop());
+        }
+        while(!stack1.isEmpty()&&stack1.peek()=='0'){
+            stack1.pop();
+        }
+        if (stack1.isEmpty()) return "0";
+        String s = "";
+        while(!stack1.isEmpty()){
+            s +=stack1.pop();
+        }
+        if (k!=0){
+            s=s.substring(0,s.length()-k);
+        }
+        return s;
     }
 }
