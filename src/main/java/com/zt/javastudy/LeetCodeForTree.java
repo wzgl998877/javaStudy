7be944d2aff53f38fefc29eae46ea689cd401580
zt <2458425315@qq.com>
2021-03-02 21:14:34 +0800

    feat() 树迭代实现永远的神

--- src/main/java/com/zt/javastudy/LeetCodeForTree.java
+++ src/main/java/com/zt/javastudy/LeetCodeForTree.java
@@ -1,6 +1,9 @@
 package com.zt.javastudy;
 
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author zhengtao
@@ -76,6 +79,115 @@ public class LeetCodeForTree {
         return root;
     }
 
+    /**
+     * 144. 二叉树的前序遍历,迭代实现
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root==null){
+            return list;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        TreeNode treeNode = root;
+        while (!queue.isEmpty()&&treeNode!=null){
+            while (treeNode!=null){
+                queue.add(treeNode.left);
+                list.add(treeNode.val);
+                treeNode = treeNode.left;
+            }
+            treeNode=queue.poll();
+            treeNode = treeNode.right;
+        }
+        return list;
+
+    }
+    /**
+     * 144. 二叉树的前序遍历,迭代实现
+     * 思路，用栈实现就可以了
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                res.add(node.val);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+        }
+        return res;
+    }
+
+    /**
+     * 迭代实现中序遍历 94、二叉树的中序遍历
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while (!stack.isEmpty()||node!=null){
+            while (node!=null){
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            while (node!=null){
+                node = node.right;
+                stack.push(node);
+            }
+            res.add(node.val);
+            node = node.right;
+        }
+        return res;
+    }
+
+    /**
+     * 145. 二叉树的后序遍历
+     * 需要用一个node来记录弹出的队列，因为如果不记录，右子树就无限循环了，建议明天继续看看
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        TreeNode flag = null;
+        while (!stack.isEmpty()||node!=null){
+            while (node!=null){
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            if(node.right==null||node.right==flag){
+                res.add(node.val);
+                flag = node;
+                node = null;
+            }else {
+                stack.push(node);
+                node = node.right;
+            }
+        }
+        return res;
+    }
 }
 
 class TreeNode {
