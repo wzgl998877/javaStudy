6fa135ec317aa8c5d25a27853db8b0899973b8e9
zt <2458425315@qq.com>
2021-03-08 20:30:51 +0800

    feat() 这一周的学习，学习了一些jvm，也学习了树的一些知识，树的题目递归肯定是最优的但是就怕不会用递归，所以迭代的题解也需要多看看

--- src/main/java/com/zt/javastudy/LeetCodeForTree.java
+++ src/main/java/com/zt/javastudy/LeetCodeForTree.java
@@ -79,38 +79,14 @@ public class LeetCodeForTree {
         return root;
     }
 
-    /**
-     * 144. 二叉树的前序遍历,迭代实现
-     * @param root
-     * @return
-     */
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        if (root==null){
-            return list;
-        }
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        TreeNode treeNode = root;
-        while (!queue.isEmpty()&&treeNode!=null){
-            while (treeNode!=null){
-                queue.add(treeNode.left);
-                list.add(treeNode.val);
-                treeNode = treeNode.left;
-            }
-            treeNode=queue.poll();
-            treeNode = treeNode.right;
-        }
-        return list;
 
-    }
     /**
      * 144. 二叉树的前序遍历,迭代实现
      * 思路，用栈实现就可以了
      * @param root
      * @return
      */
-    public List<Integer> preorderTraversal1(TreeNode root) {
+    public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<Integer>();
         if (root == null) {
             return res;
@@ -156,10 +132,29 @@ public class LeetCodeForTree {
         }
         return res;
     }
-
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while (!stack.isEmpty()||node!=null){
+            while (node!=null){
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            res.add(node.val);
+            node = node.right;
+        }
+        return res;
+    }
     /**
      * 145. 二叉树的后序遍历
      * 需要用一个node来记录弹出的队列，因为如果不记录，右子树就无限循环了，建议明天继续看看
+     * 左右中，先将左子树压入，判断右子树是否为空，如果不为空那么就压入栈，如果为空，则代表左也遍历完了，右也遍历完了，打印就可以了，
+     * 由于右子树已经遍历完了，那么就需要一个node记录，否则，右子树又会压入栈重新遍历所以就加上判断，如果如果右子树为空或者右子树已经被遍历了。
      * @param root
      * @return
      */
@@ -188,6 +183,148 @@ public class LeetCodeForTree {
         }
         return res;
     }
+
+    /**
+     * 102. 二叉树的层序遍历
+     * 由于要一层一层的输出，所以每一次遍历时需要先记录下这一层有多少数据，然后将这一层的数据加入列表就行
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null){
+            return lists;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+            int length = queue.size();
+            for (int i=0;i<length;i++){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left!=null){
+                    queue.add(node.left);
+                }
+                if (node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+
+    /**
+     * 116. 填充每个节点的下一个右侧节点指针,递归实现。
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        if (root==null){
+            return null;
+        }
+        connect(root.left,root.right);
+        return root;
+    }
+    public void connect(Node left,Node right) {
+        if (left==null||right==null){
+            return;
+        }
+        left.next = right;
+        connect(left.left,left.right);
+        connect(right.left,right.right);
+        connect(left.right,right.left);
+    }
+
+    /**
+     * 层序遍历实现，直接将每一层遍历出来，记录每一层的有多少数据
+     * 然后每一层的节点指向右节点就可以了。
+     * @param root
+     * @return
+     */
+    public Node connect1(Node root) {
+        if (root==null){
+            return null;
+        }
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int length = queue.size();
+            while (length>0){
+                Node node = queue.poll();
+                if (length>1){
+                    node.next = queue.peek();
+                }
+                if (node.left!=null){
+                    queue.add(node.left);
+                }
+                if (node.right!=null){
+                    queue.add(node.right);
+                }
+                length--;
+            }
+        }
+        return root;
+    }
+
+    /**
+     * 114. 二叉树展开为链表
+     * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+     * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+     * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+     * 这道题学习到的主要是这句话
+     * 递归算法的关键要明确函数的定义，对root的左右子树递归调用flatten函数即可：
+     * 1、这个函数的定义就是拉平二叉树，因此在递归调用后会拉平二叉树
+     * 2、通过观察发现是采用后序遍历的方法进行遍历，
+     * 3、明确要干什么，就是将左子树放在右子树上，然后将原来的右子树接在当前右子树后面
+     * @param root
+     */
+    public void flatten(TreeNode root) {
+        if (root==null){
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
+        // 二叉树已经拉平了
+        TreeNode node1 = root.left;
+        TreeNode node2 = root.right;
+        // 将左子树指向null，将右子树指向左子树
+        root.left = null;
+        root.right = node1;
+        // 将原来右子树接在当前右子树的后面
+        TreeNode treeNode = root;
+        while (treeNode.right!=null){
+            treeNode = treeNode.right;
+        }
+        treeNode.right = node2;
+    }
+
+    /**
+     * 迭代解法
+     * 当左子树不为空的时候，找到左子树中最右的节点然后将右子树连接到改节点的右子树，然后将左子树变为右子树
+     * @param root
+     */
+    public void flatten1(TreeNode root) {
+        if (root==null){
+            return;
+        }
+        TreeNode cur = root;
+        while (cur!=null){
+            if(cur.left!=null) {
+                TreeNode treeNode = cur.left;
+                TreeNode right = treeNode;
+                while (right.right != null) {
+                    right = right.right;
+                }
+                right.right = cur.right;
+                cur.left = null;
+                cur.right = treeNode;
+            }
+            cur = cur.right;
+        }
+    }
+
 }
 
 class TreeNode {
@@ -202,3 +339,22 @@ class TreeNode {
         this.right = right;
     }
  }
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
---
+++ src/main/java/com/zt/javastudy/grammar/JVMStudy.java
@@ -0,0 +1,41 @@
+package com.zt.javastudy.grammar;
+
+/**
+ * @author zhengtao
+ * @description jvm学习代码
+ * @date 2021/3/4
+ */
+public class JVMStudy {
+    public static void main(String[] args) {
+        // s位于常量池在class文件中，在类加载后就到了运行时常量池，jdk1.6后就属于了字符串常量池，所以是在堆中的
+        String s ="1234";
+        // 在jvm中第一次出现，于是s.intern会返回一个指向字符串常量池的引用指向s
+        System.out.println(s.intern()==s);
+        // 字符串常量池已经有了,不会新建对象，直接返回引用
+        String s1 = "1234";
+        // s1不是首次出现的，所以s1返回的就是指向堆里字符串常量池的引用也就是第一次出现的s
+        System.out.println(s1.intern()==s1);
+        // 因为在jvm运行时，java这个字符串就已经在字符串常量池了
+        String s2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(s2.intern()==s2);
+        // 在jvm中第一次出现，于是s3.intern只会在运行时常量池里保存第一个的引用指向s3
+        String s3 = new StringBuilder("123456").append("789").toString();
+        // 这已经不是第一次出现了，不会保存这个引用，而是继续指向s3
+        String s4 =  new StringBuilder("123456").append("789").toString();
+        System.out.println(s3.intern()==s3);
+        System.out.println(s4.intern()==s4);
+        System.out.println(s4.intern()==s3);
+        // 创建了两个对象，在直接new指令前先去字符串常量池去检查是否有这个对象如果没有那么先在字符串常量池创建这个对象然后再去堆上创建对象
+        String s5 = new String("hello");
+        // Byte,Short,Integer,Long,Character这5种整型的包装类也实现了常量池技术，很显然在堆上，默认-128 ~ 127 缓存
+        Integer a = 127;
+        Integer b = 127;
+        System.out.println(a==b);
+        Integer c = new Integer(127);
+        System.out.println(c==a);
+        // 超出了包装类的缓存区，所以在常量池里没有
+        Integer d = 128;
+        Integer e = 128;
+        System.out.println(d==e);
+    }
+}
