e499bb984edc25eec0c58e6db95a79ceb17b0b69
zt <2458425315@qq.com>
2021-03-26 19:18:34 +0800

    feat() 测试异步，异步不需要另开线程也可以的

--- src/main/java/com/zt/javastudy/JavaStudyApplication.java
+++ src/main/java/com/zt/javastudy/JavaStudyApplication.java
@@ -6,6 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author zhengtao
@@ -13,6 +19,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 // 引入lombok注解，加上这个注释就可以使用log打印日志了
 @Slf4j
+@EnableAsync
 public class JavaStudyApplication {
     public static final Logger logger = LoggerFactory.getLogger(JavaStudyApplication.class);
     public static void main(String[] args) {
@@ -22,5 +29,22 @@ public class JavaStudyApplication {
         logger.info("java-study ok.....");
         log.error("slf4j ok.........");
     }
+    /*@Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //线程池维护线程的最少数量，哪怕是空闲的
+        executor.setCorePoolSize(30);
+        //线程池维护线程的最大数量
+        executor.setMaxPoolSize(30);
+        //线程池所使用的缓冲队列，改缓冲队列的长度决定了能够缓冲的最大数量
+        executor.setQueueCapacity(200);
+        //线程池维护线程所允许的空闲时间
+        executor.setKeepAliveSeconds(100);
+        executor.setThreadNamePrefix("Asyn-TaskExecutor-");
+        executor.setRejectedExecutionHandler((Runnable r, ThreadPoolExecutor exe) -> {
+            System.out.println(("异步任务线程池队列已满"));
+        });
+        return executor;
+    }*/
 
 }
