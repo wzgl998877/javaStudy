4367b066833236dadbc0f6f18537fb1b486253e1
zt <2458425315@qq.com>
2021-04-01 21:11:40 +0800

    feat(动态规划了啊) 感觉又是一个永远的神

--- src/main/java/com/zt/javastudy/LeetCodeForDynamic.java
+++ src/main/java/com/zt/javastudy/LeetCodeForDynamic.java
@@ -1,9 +1,96 @@
 package com.zt.javastudy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author zhengtao
  * @description 动态规划
  * @date 2020/12/21
  */
 public class LeetCodeForDynamic {
+    public static void main(String[] args) {
+        System.out.println(fib1(45)%1000000008);
+        System.out.println(fib(8));
+        System.out.println(fib2(8));
+    }
+    /**
+     * 最暴力的解法，直接递归就可以
+     * @param n
+     * @return
+     */
+    public static int fib(int n) {
+        if(n == 2 || n == 1) {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+    /**
+     * 加个备忘录，防止重复计算
+     */
+    static Map<Integer, Integer> hashMap = new HashMap<>();
+
+    public static int fib1(int n) {
+        if(n == 2 || n == 1) {
+            return 1;
+        }
+       if (hashMap.containsKey(n)){
+           return hashMap.get(n);
+       }
+       int num = fib1(n-1) + fib1(n-2);
+       hashMap.put(n, num);
+       return num;
+    }
+
+    /**
+     * 动态规划
+     * @param n
+     * @return
+     */
+    public static int fib2(int n) {
+        if(n == 2 || n == 1) {
+            return 1;
+        }
+        int pre = 1, curr = 1;
+        for(int i = 3;i <= n;i++){
+            int sum = pre +curr;
+            pre = curr;
+            curr = sum;
+        }
+        return curr;
+    }
+
+    /**
+     * 322. 零钱兑换
+     * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+     *
+     * 你可以认为每种硬币的数量是无限的。
+     *
+     * 最优子结构，就相当于每一次都把最少的硬币找出来那肯定就是最少的
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        for (int i = 0; i < amount+1; i++) {
+            dp[i] = amount+1;
+        }
+        // 这个等于0很关键
+        dp[0] = 0;
+        for (int i = 0; i < dp.length; i++){
+            for (int coin : coins) {
+                if (i - coin < 0){
+                    continue;
+                }
+                // 找出最小的值来，当前是最小还是当前减去该硬币数量加一为最小
+                dp[i] = Math.min(dp[i], 1 + dp[i-coin]);
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+
+
+
 }
