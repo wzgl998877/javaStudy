93cd5f9fd5fc9ca9c91f3db63a5a61d2c5801a6b
zt <2458425315@qq.com>
2021-05-24 20:55:21 +0800

    feat() 鸡蛋还是永远的神啊，搞不懂都

--- src/main/java/com/zt/javastudy/leetcode/LeetCodeForDynamic.java
+++ src/main/java/com/zt/javastudy/leetcode/LeetCodeForDynamic.java
@@ -1880,6 +1880,121 @@ public class LeetCodeForDynamic {
         return result;
     }
 
+    /**
+     * 887. 鸡蛋掉落
+     * 给你 k 枚相同的鸡蛋，并可以使用一栋从第 1 层到第 n 层共有 n 层楼的建筑。
+     *
+     * 已知存在楼层 f ，满足 0 <= f <= n ，任何从 高于 f 的楼层落下的鸡蛋都会碎，从 f 楼层或比它低的楼层落下的鸡蛋都不会破。
+     *
+     * 每次操作，你可以取一枚没有碎的鸡蛋并把它从任一楼层 x 扔下（满足 1 <= x <= n）。如果鸡蛋碎了，你就不能再次使用它。如果某枚鸡蛋扔下后没有摔碎，则可以在之后的操作中 重复使用 这枚鸡蛋。
+     *
+     * 请你计算并返回要确定 f 确切的值 的 最小操作次数 是多少？
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：k = 1, n = 2
+     * 输出：2
+     * 解释：
+     * 鸡蛋从 1 楼掉落。如果它碎了，肯定能得出 f = 0 。
+     * 否则，鸡蛋从 2 楼掉落。如果它碎了，肯定能得出 f = 1 。
+     * 如果它没碎，那么肯定能得出 f = 2 。
+     * 因此，在最坏的情况下我们需要移动 2 次以确定 f 是多少。
+     * 示例 2：
+     *
+     * 输入：k = 2, n = 6
+     * 输出：3
+     * 示例 3：
+     *
+     * 输入：k = 3, n = 14
+     * 输出：4
+     * @param k
+     * @param n
+     * @return
+     */
+    public int superEggDropMy(int k, int n) {
+        // dp[i][j]的定义是用i枚鸡蛋扔j层楼找到最少次数
+        int[][] dp = new int[k+1][n+1];
+        for(int i = 0; i <= k; i++){
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= n; i++){
+            dp[1][i] = i;
+            dp[0][i] = 0;
+        }
+        for(int i = 2; i <= k; i++){
+            for(int j = 1; j <= n; j++){
+                int min = j*j;
+                for (int x = 1; x <= j; x++){
+                    min = Math.min(Math.max(dp[i-1][x-1], dp[i][j-x]) + 1, min);
+                }
+                dp[i][j] = min;
+            }
+        }
+        return dp[k][n];
+    }
+    Map<Integer, Integer> cache = new HashMap<>();
+    public int superEggDrop(int k, int n) {
+        if (n == 0)
+            return 0;
+        else if (k == 1)
+            return n;
+
+        Integer key = n * 1000 + k; // K <= 100
+        if (cache.containsKey(key))
+            return cache.get(key);
+
+        int low = 1, high = n;
+        while (low + 1 < high) {
+            int middle = (low + high) / 2;
+            int lowVal = superEggDrop(k - 1, middle - 1);
+            int highVal = superEggDrop(k, n - middle);
+
+            if (lowVal < highVal)
+                low = middle;
+            else if (lowVal > highVal)
+                high = middle;
+            else
+                low = high = middle;
+        }
+        int minimum = 1 + Math.min(
+                Math.max(superEggDrop(k - 1, low - 1), superEggDrop(k, n - low)),
+                Math.max(superEggDrop(k - 1, high - 1), superEggDrop(k, n - high))
+        );
+
+        cache.put(key, minimum);
+
+        return cache.get(key);
+    }
+
+    /**
+     * 这是人做的吗
+     * @param k
+     * @param n
+     * @return
+     */
+    public int superEggDrop1(int k, int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[][] f = new int[n + 1][k + 1];
+        for (int i = 1; i <= k; ++i) {
+            f[1][i] = 1;
+        }
+        int ans = -1;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= k; ++j) {
+                f[i][j] = 1 + f[i - 1][j - 1] + f[i - 1][j];
+            }
+            if (f[i][k] >= n) {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+
+
 
 
 
