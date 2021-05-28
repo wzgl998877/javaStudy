5ca8b7c7b5b8506a5013b01614d98196256e27cc
zt <2458425315@qq.com>
2021-05-28 17:41:54 +0800

    feat() 正则匹配也是永远的神讲道理

--- src/main/java/com/zt/javastudy/leetcode/LeetCodeForDynamic.java
+++ src/main/java/com/zt/javastudy/leetcode/LeetCodeForDynamic.java
@@ -1994,6 +1994,117 @@ public class LeetCodeForDynamic {
         return ans;
     }
 
+    /**
+     * 10. 正则表达式匹配
+     * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+     *
+     * '.' 匹配任意单个字符
+     * '*' 匹配零个或多个前面的那一个元素
+     * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：s = "aa" p = "a"
+     * 输出：false
+     * 解释："a" 无法匹配 "aa" 整个字符串。
+     * 示例 2:
+     *
+     * 输入：s = "aa" p = "a*"
+     * 输出：true
+     * 解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+     * 示例 3：
+     *
+     * 输入：s = "ab" p = ".*"
+     * 输出：true
+     * 解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+     * 示例 4：
+     *
+     * 输入：s = "aab" p = "c*a*b"
+     * 输出：true
+     * 解释：因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+     * 示例 5：
+     *
+     * 输入：s = "mississippi" p = "mis*is*p*."
+     * 输出：false
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch(String s, String p) {
+        // 思路是正确的但是写不出来啊
+        if (p.isEmpty()) {
+            return s.isEmpty();
+        }
+        boolean match = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+        if(p.length() >= 2 && p.charAt(1) == '*'){
+            return isMatch(s, p.substring(2)) || (match && isMatch(s.substring(1), p));
+        }
+        return match && isMatch(s.substring(1), p.substring(1));
+    }
+
+    public boolean isMatch1(String s, String p) {
+        // 递归加上备忘录
+        int[][] memo = new int[s.length() + 1][p.length() + 1];
+        return isMatch(s, 0, p, 0, memo);
+    }
+    public boolean isMatch(String s, int i, String p, int j, int[][] memo){
+        if (j >= p.length()){
+            return i >= s.length();
+        }
+        if (memo[i][j] != 0){
+            return memo[i][j] > 0;
+        }
+        boolean match = i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+        if (p.length() - j >= 2 && p.charAt(j+1) == '*'){
+            boolean t = isMatch(s, i, p, j + 2, memo) || (match && isMatch(s, i + 1, p, j, memo));
+            if (t){
+                memo[i][j] = 1;
+            } else {
+                memo[i][j] = -1;
+            }
+            return t;
+        }
+        boolean t = match && isMatch(s, i + 1, p, j + 1, memo);
+        if (t){
+            memo[i][j] = 1;
+        } else {
+            memo[i][j] = -1;
+        }
+        return t;
+    }
+
+    /**
+     * 动态规划解法
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch2(String s, String p) {
+        // dp的定义dp[i][j] 代表从dp[0..i][0..j] 能否被匹配
+        char[] str = s.toCharArray(), ptr = p.toCharArray();
+        boolean[][] dp = new boolean[str.length + 1][ptr.length + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= str.length; i++) {
+            for (int j = 1; j <= ptr.length; j++) {
+                if(ptr[j - 1] != '*') {
+                    if(i > 0 && (str[i - 1] == ptr[j - 1] || ptr[j - 1] == '.')) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }else { //ptr[j - 1] == '*'
+                    if(j > 1) {
+                        dp[i][j] |= dp[i][j - 2];   //不看
+                    }
+                    if(i > 0 && j > 1 && (str[i - 1] == ptr[j - 2] || ptr[j - 2] == '.')) {
+                        dp[i][j] |= dp[i - 1][j];    //看
+                    }
+                }
+            }
+        }
+        return dp[str.length][ptr.length];
+    }
+
+
 
 
 
