384ec8e724766ce2c12fc9ca781edbb775a03a11
zt <2458425315@qq.com>
2021-07-09 16:09:11 +0800

    feat() 既然做了两道题，用的是动态规划但是题解都教我贪心，那就试试贪心吧

--- src/main/java/com/zt/javastudy/leetcode/LeetCodeForDynamic.java
+++ src/main/java/com/zt/javastudy/leetcode/LeetCodeForDynamic.java
@@ -79,6 +79,8 @@ public class LeetCodeForDynamic {
         System.out.println(stoneGameV(stone));
         String not = "zjveiiwvc";
         System.out.println(minInsertions(not));
+        int[] jumps = {2,3,0,1,4};
+        System.out.println(jump(jumps));
     }
 
     /**
@@ -2964,6 +2966,56 @@ public class LeetCodeForDynamic {
             max = Math.max(max, i + nums[i]);
         }
         return true;
+    }
+
+    /**
+     * 45. 跳跃游戏 II
+     * 给定一个非负整数数组，你最初位于数组的第一个位置。
+     * <p>
+     * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+     * <p>
+     * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+     * <p>
+     * 假设你总是可以到达数组的最后一个位置。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: [2,3,1,1,4]
+     * 输出: 2
+     * 解释: 跳到最后一个位置的最小跳跃数是 2。
+     * 从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+     * 示例 2:
+     * <p>
+     * 输入: [2,3,0,1,4]
+     * 输出: 2
+     * <p>
+     * <p>
+     * 提示:
+     * <p>
+     * 1 <= nums.length <= 1000
+     * 0 <= nums[i] <= 105
+     *
+     * @param nums
+     * @return
+     */
+    public static int jump(int[] nums) {
+        int m = nums.length;
+        int[] dp = new int[m];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < i; j++) {
+                if (j + nums[j] >= i) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                    break;
+                }
+            }
+            System.out.println(Arrays.toString(dp));
+        }
+
+        return dp[m - 1];
     }
 
 
