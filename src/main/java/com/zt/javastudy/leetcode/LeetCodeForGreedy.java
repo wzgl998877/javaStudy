8c747743ac6a177a27e7576be2ecc4c4efe3e748
zt <2458425315@qq.com>
2021-08-10 18:14:44 +0800

    feat(): 贪心刷题

--- src/main/java/com/zt/javastudy/leetcode/LeetCodeForGreedy.java
+++ src/main/java/com/zt/javastudy/leetcode/LeetCodeForGreedy.java
@@ -20,6 +20,13 @@ public class LeetCodeForGreedy {
         System.out.println(intervalIntersection1(clips, clips2));
         int[] ratings = {1, 2, 87, 87, 87, 2, 1};
         System.out.println(candy(ratings));
+        String s = "ababcbacadefegdehijhklij";
+        System.out.println(partitionLabels(s));
+        System.out.println(monotoneIncreasingDigits(332));
+        char c = '2';
+        System.out.println(c - 1);
+        System.out.println(c - '1');
+
     }
 
     /**
@@ -626,6 +633,7 @@ public class LeetCodeForGreedy {
         }
         return n;
     }
+
     public int candy1(int[] ratings) {
         int n = ratings.length;
         int ret = 1;
@@ -646,6 +654,185 @@ public class LeetCodeForGreedy {
             }
         }
         return ret;
+    }
+
+    /**
+     * 763. 划分字母区间
+     * 字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+     * <p>
+     * <p>
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：S = "ababcbacadefegdehijhklij"
+     * 输出：[9,7,8]
+     * 解释：
+     * 划分结果为 "ababcbaca", "defegde", "hijhklij"。
+     * 每个字母最多出现在一个片段中。
+     * 像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
+     * <p>
+     * <p>
+     * 提示：
+     * <p>
+     * S的长度在[1, 500]之间。
+     * S只包含小写字母 'a' 到 'z' 。
+     *
+     * @param s
+     * @return
+     */
+    public static List<Integer> partitionLabels(String s) {
+        List<Integer> list = new ArrayList<>();
+        char[] c = s.toCharArray();
+        int temp = s.lastIndexOf(c[0]), pre = 0;
+        for (int i = 0; i < c.length; i++) {
+            if (temp >= i) {
+                temp = Math.max(temp, s.lastIndexOf(c[i]));
+            } else {
+                list.add(temp + 1 - pre);
+                pre = temp + 1;
+                temp = s.lastIndexOf(c[i]);
+            }
+        }
+        list.add(s.length() - pre);
+        return list;
+    }
+
+    public List<Integer> partitionLabels1(String s) {
+        int[] last = new int[26];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+        List<Integer> partition = new ArrayList<Integer>();
+        int start = 0, end = 0;
+        for (int i = 0; i < length; i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if (i == end) {
+                partition.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
+    }
+
+    /**
+     * 376. 摆动序列
+     * 如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为 摆动序列 。第一个差（如果存在的话）可能是正数或负数。仅有一个元素或者含两个不等元素的序列也视作摆动序列。
+     * <p>
+     * 例如， [1, 7, 4, 9, 2, 5] 是一个 摆动序列 ，因为差值 (6, -3, 5, -7, 3) 是正负交替出现的。
+     * <p>
+     * 相反，[1, 4, 7, 2, 5] 和 [1, 7, 4, 5, 5] 不是摆动序列，第一个序列是因为它的前两个差值都是正数，第二个序列是因为它的最后一个差值为零。
+     * 子序列 可以通过从原始序列中删除一些（也可以不删除）元素来获得，剩下的元素保持其原始顺序。
+     * <p>
+     * 给你一个整数数组 nums ，返回 nums 中作为 摆动序列 的 最长子序列的长度 。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：nums = [1,7,4,9,2,5]
+     * 输出：6
+     * 解释：整个序列均为摆动序列，各元素之间的差值为 (6, -3, 5, -7, 3) 。
+     * 示例 2：
+     * <p>
+     * 输入：nums = [1,17,5,10,13,15,10,5,16,8]
+     * 输出：7
+     * 解释：这个序列包含几个长度为 7 摆动序列。
+     * 其中一个是 [1, 17, 10, 13, 10, 16, 8] ，各元素之间的差值为 (16, -7, 3, -3, 6, -8) 。
+     * 示例 3：
+     * <p>
+     * 输入：nums = [1,2,3,4,5,6,7,8,9]
+     * 输出：2
+     * <p>
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= nums.length <= 1000
+     * 0 <= nums[i] <= 1000
+     *
+     * @param nums
+     * @return
+     */
+    public int wiggleMaxLength(int[] nums) {
+        int n = nums.length;
+        if (n <= 1) {
+            return n;
+        }
+        int now = 0;
+        int temp = 0;
+        int result = 1;
+        for (int i = 1; i < nums.length; i++) {
+            now = nums[i] - nums[i - 1];
+            if ((now > 0 && temp <= 0) || (now < 0 && temp >= 0)) {
+                result++;
+                temp = now;
+            }
+        }
+        return result;
+    }
+
+    public int wiggleMaxLength1(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        // up[i] 表示以前 i 个元素中的某一个为结尾的最长的「上升摆动序列」的长度。
+        // down[i] 表示以前 i 个元素中的某一个为结尾的最长的「下降摆动序列」的长度。
+        int[] up = new int[n];
+        int[] down = new int[n];
+        up[0] = down[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = Math.max(up[i - 1], down[i - 1] + 1);
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                up[i] = up[i - 1];
+                down[i] = Math.max(up[i - 1] + 1, down[i - 1]);
+            } else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
+            }
+        }
+        return Math.max(up[n - 1], down[n - 1]);
+    }
+
+    /**
+     * 738. 单调递增的数字
+     * 给定一个非负整数 N，找出小于或等于 N 的最大的整数，同时这个整数需要满足其各个位数上的数字是单调递增。
+     * <p>
+     * （当且仅当每个相邻位数上的数字 x 和 y 满足 x <= y 时，我们称这个整数是单调递增的。）
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: N = 10
+     * 输出: 9
+     * 示例 2:
+     * <p>
+     * 输入: N = 1234
+     * 输出: 1234
+     * 示例 3:
+     * <p>
+     * 输入: N = 332
+     * 输出: 299
+     * 说明: N 是在 [0, 10^9] 范围内的一个整数。
+     *
+     * @param N
+     * @return
+     */
+    public static int monotoneIncreasingDigits(int N) {
+        // 这题的思路就是如果有一个数不是递增的，就把这个数减一，其他的置为9，但是需要倒过来遍历
+        char[] s = String.valueOf(N).toCharArray();
+        int flag = s.length;
+        for (int i = s.length - 1; i >= 1; i--) {
+            if (s[i] < s[i - 1]) {
+                flag = i;
+                s[i - 1] --;
+            }
+        }
+        for (int i = flag; i < s.length; i++){
+            s[i] = '9';
+        }
+        return Integer.parseInt(String.valueOf(s));
     }
 }
 
