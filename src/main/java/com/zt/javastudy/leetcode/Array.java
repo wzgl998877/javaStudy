b9ebaa8418a6bd8f0d6496651e3e9588a839e2dd
zt <2458425315@qq.com>
2021-12-21 23:54:20 +0800

    feat(): o(1) 时间完成增删改查，原理为使用arraylist当容器，使用map记录索引值

--- src/main/java/com/zt/javastudy/leetcode/Array.java
+++ src/main/java/com/zt/javastudy/leetcode/Array.java
@@ -65,6 +65,10 @@ public class Array {
         System.out.println();
         int[] threeSum = {1,2,-2,-1};
         System.out.println(array.threeSum(threeSum));
+        RandomizedCollection randomizedCollection = new RandomizedCollection();
+        System.out.println(randomizedCollection.insert(1));
+        System.out.println(randomizedCollection.remove(1));
+        System.out.println(randomizedCollection.insert(1));
     }
 
     /**
@@ -1840,6 +1844,85 @@ public class Array {
             }
         }
         return result;
+    }
+
+    /**
+     * 381. O(1) 时间插入、删除和获取随机元素 - 允许重复
+     * 设计一个支持在平均 时间复杂度 O(1) 下， 执行以下操作的数据结构。
+     *
+     * 注意: 允许出现重复元素。
+     *
+     * insert(val)：向集合中插入元素 val。
+     * remove(val)：当 val 存在时，从集合中移除一个 val。
+     * getRandom：从现有集合中随机获取一个元素。每个元素被返回的概率应该与其在集合中的数量呈线性相关。
+     * 示例:
+     *
+     * // 初始化一个空的集合。
+     * RandomizedCollection collection = new RandomizedCollection();
+     *
+     * // 向集合中插入 1 。返回 true 表示集合不包含 1 。
+     * collection.insert(1);
+     *
+     * // 向集合中插入另一个 1 。返回 false 表示集合包含 1 。集合现在包含 [1,1] 。
+     * collection.insert(1);
+     *
+     * // 向集合中插入 2 ，返回 true 。集合现在包含 [1,1,2] 。
+     * collection.insert(2);
+     *
+     * // getRandom 应当有 2/3 的概率返回 1 ，1/3 的概率返回 2 。
+     * collection.getRandom();
+     *
+     * // 从集合中删除 1 ，返回 true 。集合现在包含 [1,2] 。
+     * collection.remove(1);
+     *
+     * // getRandom 应有相同概率返回 1 和 2 。
+     * collection.getRandom();
+     * 这种题目的思路
+     * 1：0(1)时间插入查询，用arraylist，或者hashMap
+     * 2，删除，用数组删除要是o（1），则必须把要删除的元素换到最后一位
+     * 3、所以需要使用map记录每个元素的索引，以便换时能找到换的元素
+     */
+    static class RandomizedCollection {
+        private Map<Integer, Set<Integer>> map;
+        private List<Integer> list;
+        Random random = new Random();
+        public RandomizedCollection() {
+            this.map = new HashMap<>();
+            this.list = new ArrayList<>();
+        }
+
+        public boolean insert(int val) {
+            list.add(val);
+            Set<Integer> set = map.getOrDefault(val, new HashSet<>());
+            set.add(list.size() - 1);
+            map.put(val, set);
+            return set.size() == 1;
+        }
+
+        public boolean remove(int val) {
+            if(!map.containsKey(val)) {
+                return false;
+            }
+            Set<Integer> set = map.get(val);
+            Iterator<Integer> iterator = set.iterator();
+            int index = iterator.next();
+            int lastNum = list.get(list.size() - 1);
+            list.set(index, lastNum);
+            map.get(lastNum).remove(list.size() - 1);
+            set.remove(index);
+            if(index < list.size() - 1) {
+                map.get(lastNum).add(index);
+            }
+            if(set.size() == 0) {
+                map.remove(val);
+            }
+            list.remove(list.size() - 1);
+            return true;
+        }
+
+        public int getRandom() {
+            return list.get(random.nextInt(list.size()));
+        }
     }
 
 }
