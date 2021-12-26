9fcaf2e04f95d131e1fe625c0c04db7f63ae55f4
zt <2458425315@qq.com>
2021-12-27 00:27:55 +0800

    feat(): 这题太难了，抄答案的

--- src/main/java/com/zt/javastudy/leetcode/Array.java
+++ src/main/java/com/zt/javastudy/leetcode/Array.java
@@ -72,6 +72,12 @@ public class Array {
         int[] remove = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         System.out.println(array.removeElement(remove, 2));
         System.out.println(array.removeDuplicates(remove));
+        int[] sort = {-7, -3, 2, 3, 11};
+        System.out.println(Arrays.toString(array.sortedSquares(sort)));
+        int[] nums1 = {3, 4, 6, 5};
+        int[] nums2 = {9, 1, 2, 5, 8, 3};
+        System.out.println(Arrays.toString(array.maxNumber(nums1, nums2, 5)));
+
     }
 
     /**
@@ -2083,12 +2089,12 @@ public class Array {
     public void moveZeroes(int[] nums) {
         int low = 0;
         for (int fast = 0; fast < nums.length; fast++) {
-            if(nums[fast] != 0) {
+            if (nums[fast] != 0) {
                 nums[low] = nums[fast];
                 low++;
             }
         }
-        for(int i = low + 1; i < nums.length; i++) {
+        for (int i = low + 1; i < nums.length; i++) {
             nums[i] = 0;
         }
     }
@@ -2096,7 +2102,7 @@ public class Array {
     public void moveZeroes1(int[] nums) {
         int low = 0;
         for (int fast = 0; fast < nums.length; fast++) {
-            if(nums[fast] != 0) {
+            if (nums[fast] != 0) {
                 int temp = nums[low];
                 nums[low] = nums[fast];
                 nums[fast] = temp;
@@ -2104,5 +2110,160 @@ public class Array {
             }
         }
     }
+
+    /**
+     * 977. 有序数组的平方
+     * 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：nums = [-4,-1,0,3,10]
+     * 输出：[0,1,9,16,100]
+     * 解释：平方后，数组变为 [16,1,0,9,100]
+     * 排序后，数组变为 [0,1,9,16,100]
+     * 示例 2：
+     * <p>
+     * 输入：nums = [-7,-3,2,3,11]
+     * 输出：[4,9,9,49,121]
+     * <p>
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= nums.length <= 104
+     * -104 <= nums[i] <= 104
+     * nums 已按 非递减顺序 排序
+     *
+     * @param nums
+     * @return
+     */
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1, index = n - 1;
+        while (left <= right) {
+            int a = nums[left] * nums[left];
+            int b = nums[right] * nums[right];
+            if (a >= b) {
+                result[index--] = a;
+                left++;
+            } else {
+                result[index--] = b;
+                right--;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 321. 拼接最大数
+     * 给定长度分别为 m 和 n 的两个数组，其元素由 0-9 构成，表示两个自然数各位上的数字。现在从这两个数组中选出 k (k <= m + n) 个数字拼接成一个新的数，要求从同一个数组中取出的数字保持其在原数组中的相对顺序。
+     * <p>
+     * 求满足该条件的最大数。结果返回一个表示该最大数的长度为 k 的数组。
+     * <p>
+     * 说明: 请尽可能地优化你算法的时间和空间复杂度。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入:
+     * nums1 = [3, 4, 6, 5]
+     * nums2 = [9, 1, 2, 5, 8, 3]
+     * k = 5
+     * 输出:
+     * [9, 8, 6, 5, 3]
+     * 示例 2:
+     * <p>
+     * 输入:
+     * nums1 = [6, 7]
+     * nums2 = [6, 0, 4]
+     * k = 5
+     * 输出:
+     * [6, 7, 6, 0, 4]
+     * 示例 3:
+     * <p>
+     * 输入:
+     * nums1 = [3, 9]
+     * nums2 = [8, 9]
+     * k = 3
+     * 输出:
+     * [9, 8, 9]
+     *
+     * @param nums1
+     * @param nums2
+     * @param k
+     * @return 为了找到长度为 k 的最大数，
+     * 需要从两个数组中分别选出最大的子序列，
+     * 1、这两个子序列的长度之和为 k，
+     * 2、然后将这两个子序列合并得到最大数。
+     */
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length, n = nums2.length;
+        int[] maxSubsequence = new int[k];
+        int start = Math.max(0, k - n), end = Math.min(k, m);
+        for (int i = start; i <= end; i++) {
+            int[] subsequence1 = maxSequence(nums1, i);
+            int[] subsequence2 = maxSequence(nums2, k - i);
+            int[] curMaxSubsequence = merge(subsequence1, subsequence2);
+            if (compare(curMaxSubsequence, 0, maxSubsequence, 0) > 0) {
+                System.arraycopy(curMaxSubsequence, 0, maxSubsequence, 0, k);
+            }
+        }
+        return maxSubsequence;
+    }
+
+    public int[] maxSequence(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[k];
+        int top = -1;
+        int remain = n - k;
+        for(int i = 0; i < n; i++) {
+            int temp = nums[i];
+            while (top >= 0 && temp > result[top] && remain > 0) {
+                top--;
+                remain--;
+            }
+            if(top < k - 1) {
+                result[++top] = temp;
+            } else {
+                remain--;
+            }
+        }
+        return result;
+    }
+    public int[] merge(int[] subsequence1, int[] subsequence2) {
+        int x = subsequence1.length, y = subsequence2.length;
+        if (x == 0) {
+            return subsequence2;
+        }
+        if (y == 0) {
+            return subsequence1;
+        }
+        int mergeLength = x + y;
+        int[] merged = new int[mergeLength];
+        int index1 = 0, index2 = 0;
+        for (int i = 0; i < mergeLength; i++) {
+            if (compare(subsequence1, index1, subsequence2, index2) > 0) {
+                merged[i] = subsequence1[index1++];
+            } else {
+                merged[i] = subsequence2[index2++];
+            }
+        }
+        return merged;
+    }
+
+    public int compare(int[] subsequence1, int index1, int[] subsequence2, int index2) {
+        int x = subsequence1.length, y = subsequence2.length;
+        while (index1 < x && index2 < y) {
+            int difference = subsequence1[index1] - subsequence2[index2];
+            if (difference != 0) {
+                return difference;
+            }
+            index1++;
+            index2++;
+        }
+        return (x - index1) - (y - index2);
+    }
+
 
 }
