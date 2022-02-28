9d18588fb541aafbc7199d700d06e8e362a91e19
zt <2458425315@qq.com>
2022-03-01 00:06:18 +0800

    feat(): 一模一样的几题，主要看424

--- src/main/java/com/zt/javastudy/leetcode/Array.java
+++ src/main/java/com/zt/javastudy/leetcode/Array.java
@@ -1816,6 +1816,48 @@ public class Array {
     }
 
     /**
+     * 480. 滑动窗口中位数
+     * 中位数是有序序列最中间的那个数。如果序列的长度是偶数，则没有最中间的数；此时中位数是最中间的两个数的平均数。
+     * <p>
+     * 例如：
+     * <p>
+     * [2,3,4]，中位数是 3
+     * [2,3]，中位数是 (2 + 3) / 2 = 2.5
+     * 给你一个数组 nums，有一个长度为 k 的窗口从最左端滑动到最右端。窗口中有 k 个数，每次窗口向右移动 1 位。你的任务是找出每次窗口移动后得到的新窗口中元素的中位数，并输出由它们组成的数组。
+     * <p>
+     * <p>
+     * <p>
+     * 示例：
+     * <p>
+     * 给出 nums = [1,3,-1,-3,5,3,6,7]，以及 k = 3。
+     * <p>
+     * 窗口位置                      中位数
+     * ---------------               -----
+     * [1  3  -1] -3  5  3  6  7       1
+     * 1 [3  -1  -3] 5  3  6  7      -1
+     * 1  3 [-1  -3  5] 3  6  7      -1
+     * 1  3  -1 [-3  5  3] 6  7       3
+     * 1  3  -1  -3 [5  3  6] 7       5
+     * 1  3  -1  -3  5 [3  6  7]      6
+     * 因此，返回该滑动窗口的中位数数组 [1,-1,-1,3,5,6]。
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        double[] result = new double[nums.length - k + 1];
+        int left = 0, right = 0;
+        while(right < nums.length) {
+            right++;
+            if(right - left == k) {
+
+            }
+        }
+        return result;
+    }
+
+    /**
      * 424. 替换后的最长重复字符
      * 给你一个字符串 s 和一个整数 k 。你可以选择字符串中的任一字符，并将其更改为任何其他大写英文字符。该操作最多可执行 k 次。
      * <p>
@@ -2003,6 +2045,54 @@ public class Array {
                 left++;
             }
             result = Math.max(result, right - left - 1);
+        }
+        return result;
+    }
+
+    /**
+     * 1838. 最高频元素的频数
+     * 元素的 频数 是该元素在一个数组中出现的次数。
+     * <p>
+     * 给你一个整数数组 nums 和一个整数 k 。在一步操作中，你可以选择 nums 的一个下标，并将该下标对应元素的值增加 1 。
+     * <p>
+     * 执行最多 k 次操作后，返回数组中最高频元素的 最大可能频数 。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：nums = [1,2,4], k = 5
+     * 输出：3
+     * 解释：对第一个元素执行 3 次递增操作，对第二个元素执 2 次递增操作，此时 nums = [4,4,4] 。
+     * 4 是数组中最高频元素，频数是 3 。
+     * 示例 2：
+     * <p>
+     * 输入：nums = [1,4,8,13], k = 5
+     * 输出：2
+     * 解释：存在多种最优解决方案：
+     * - 对第一个元素执行 3 次递增操作，此时 nums = [4,4,8,13] 。4 是数组中最高频元素，频数是 2 。
+     * - 对第二个元素执行 4 次递增操作，此时 nums = [1,8,8,13] 。8 是数组中最高频元素，频数是 2 。
+     * - 对第三个元素执行 5 次递增操作，此时 nums = [1,4,13,13] 。13 是数组中最高频元素，频数是 2 。
+     * 示例 3：
+     * <p>
+     * 输入：nums = [3,9,6], k = 2
+     * 输出：1
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 0, right = 1, count = 0, result = 0;
+        while (right < nums.length) {
+            count += (nums[right] - nums[right - 1]) * (right - left);
+            right++;
+            while (count > k) {
+                count -= nums[right - 1] - nums[left];
+                left++;
+            }
+            result = Math.max(result, right - left);
         }
         return result;
     }
