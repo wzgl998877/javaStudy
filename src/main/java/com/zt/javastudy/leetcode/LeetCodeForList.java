293df83c4f6039f8f3ce6508d0292258df0140f9
zhengtao <2458425315@qq.com>
2022-06-16 00:26:03 +0800

    feat(): 快慢指针

--- src/main/java/com/zt/javastudy/leetcode/LeetCodeForList.java
+++ src/main/java/com/zt/javastudy/leetcode/LeetCodeForList.java
@@ -837,6 +837,124 @@ public class LeetCodeForList {
         return low;
     }
 
+    /**
+     * 160. 相交链表
+     * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+     *
+     * 图示两个链表在节点 c1 开始相交：
+     *
+     *
+     *
+     * 题目数据 保证 整个链式结构中不存在环。
+     *
+     * 注意，函数返回结果后，链表必须 保持其原始结构 。
+     *
+     * 自定义评测：
+     *
+     * 评测系统 的输入如下（你设计的程序 不适用 此输入）：
+     *
+     * intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+     * listA - 第一个链表
+     * listB - 第二个链表
+     * skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+     * skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+     * 评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     *
+     *
+     * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+     * 输出：Intersected at '8'
+     * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+     * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,6,1,8,4,5]。
+     * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+     * 示例 2：
+     *
+     *
+     *
+     * 输入：intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+     * 输出：Intersected at '2'
+     * 解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
+     * 从各自的表头开始算起，链表 A 为 [1,9,1,2,4]，链表 B 为 [3,2,4]。
+     * 在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+     * 示例 3：
+     *
+     *
+     *
+     * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+     * 输出：null
+     * 解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。
+     * 由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+     * 这两个链表不相交，因此返回 null 。
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA, p2 = headB;
+        while(p1 != p2) {
+            // p1遍历完链表A之后开始遍历链表B
+            p1 = p1 != null ? p1.next : headB;
+            // p2遍历完链表B之后开始遍历链表A
+            p2 = p2 != null ? p2.next : headA;
+        }
+        return p1;
+    }
+
+    /**
+     * 206. 反转链表
+     * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+     *
+     *
+     * 示例 1：
+     *
+     *
+     * 输入：head = [1,2,3,4,5]
+     * 输出：[5,4,3,2,1]
+     * 示例 2：
+     *
+     *
+     * 输入：head = [1,2]
+     * 输出：[2,1]
+     * 示例 3：
+     * 思路：
+     * 定义两个指针： p1 和 p2 ；p1 在前 p2 在后。
+     * 每次让 p1 的 next 指向 p2 ，实现一次局部反转
+     * 局部反转完成之后，p1 和 p2 同时往前移动一个位置
+     * 输入：head = []
+     * 输出：[]
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode p1 = head, p2 = null;
+        while(p1 != null) {
+            ListNode temp = p1.next;
+            // p1 的 next 指向 p2 ，实现一次局部反转
+            p1.next = p2;
+            // p2往前移动一个位置
+            p2 = p1;
+            // p1往前移动一个位置
+            p1 = temp;
+        }
+        return p2;
+    }
+    public ListNode reverseList1(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode result = reverseList(head.next);
+        // 翻转head和head.next, 可以看成（head.next）.next = head
+        head.next.next = head;
+        // 尾结点指向null
+        head.next = null;
+        return result;
+    }
+
 }
 
 
