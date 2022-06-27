2ab68d84f95dff296a0fa9be4ebc30a2c27ce257
zhengtao <2458425315@qq.com>
2022-06-28 00:40:20 +0800

    feat(): 两道简单题

--- src/main/java/com/zt/javastudy/leetcode/LeetCodeForList.java
+++ src/main/java/com/zt/javastudy/leetcode/LeetCodeForList.java
@@ -1097,6 +1097,56 @@ public class LeetCodeForList {
         return newHead;
     }
 
+    /**
+     * 203. 移除链表元素
+     * 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * <p>
+     * 输入：head = [1,2,6,3,4,5,6], val = 6
+     * 输出：[1,2,3,4,5]
+     * 示例 2：
+     * <p>
+     * 输入：head = [], val = 1
+     * 输出：[]
+     * 示例 3：
+     * <p>
+     * 输入：head = [7,7,7,7], val = 7
+     * 输出：[]
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode node = new ListNode(-1, head);
+        ListNode temp = node;
+        while (temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return node.next;
+    }
+
+    /**
+     * 递归
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements1(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        head.next = removeElements1(head, val);
+        return head.val == val ? head.next : head;
+    }
 
 }
 
