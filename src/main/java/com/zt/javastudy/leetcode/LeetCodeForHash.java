e14e89efc63f5d628e74a4254e43f2a6f057849e
zhengtao <2458425315@qq.com>
2022-07-12 08:46:06 +0800

    feat(): 哈希表。。。

--- src/main/java/com/zt/javastudy/leetcode/LeetCodeForHash.java
+++ src/main/java/com/zt/javastudy/leetcode/LeetCodeForHash.java
@@ -116,4 +116,68 @@ public class LeetCodeForHash {
         return Arrays.copyOfRange(intersection, 0, index);
     }
 
+    /**
+     * 202. 快乐数
+     * 编写一个算法来判断一个数 n 是不是快乐数。
+     *
+     * 「快乐数」 定义为：
+     *
+     * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+     * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+     * 如果这个过程 结果为 1，那么这个数就是快乐数。
+     * 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：n = 19
+     * 输出：true
+     * 解释：
+     * 12 + 92 = 82
+     * 82 + 22 = 68
+     * 62 + 82 = 100
+     * 12 + 02 + 02 = 1
+     * 示例 2：
+     *
+     * 输入：n = 2
+     * 输出：false
+     * @param n
+     * @return
+     */
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while(n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    /**
+     * 快慢指针
+     * @param n
+     * @return
+     */
+    public boolean isHappy1(int n) {
+        int fast = getNext(getNext(n));
+        int slow = getNext(n);
+        while(fast != 1 && slow != 1) {
+            if(fast == slow) {
+                return false;
+            }
+            fast = getNext(getNext(fast));
+            slow = getNext(slow);
+        }
+        return true;
+    }
+    public int getNext(int n) {
+        int result = 0;
+        while(n != 0) {
+            result += (n % 10)*(n % 10);
+            n = n / 10;
+        }
+        return result;
+    }
+
 }
