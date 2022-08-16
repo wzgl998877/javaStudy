1600e7377f2ab6626eaaad8792f03238e96b8c75
zhengtao <2458425315@qq.com>
2022-08-17 00:26:10 +0800

    feat(): 数组，好久没刷题了，最近要刷起来了

--- src/main/java/com/zt/javastudy/leetcode/LeetCodeForString.java
+++ src/main/java/com/zt/javastudy/leetcode/LeetCodeForString.java
@@ -9,6 +9,9 @@ public class LeetCodeForString {
     public static void main(String[] args) {
         LeetCodeForString leetCodeForString = new LeetCodeForString();
         System.out.println(leetCodeForString.reverseStr("krmyfshbspcgtesxnnljhfursyissjnsocgdhgfxubewllxzqhpasguvlrxtkgatzfybprfmmfithphckksnvjkcvnsqgsgosfxc", 20));
+        System.out.println(leetCodeForString.reverseWords("  hello  world  "));
+        System.out.println(leetCodeForString.reverseWords1("  hello  world  "));
+        System.out.println(leetCodeForString.reverseWords2("  hello  world  "));
     }
 
     /**
@@ -68,6 +71,128 @@ public class LeetCodeForString {
             c[right] = temp;
             left++;
             right--;
+        }
+    }
+
+    /**
+     * 151. 颠倒字符串中的单词
+     * 给你一个字符串 s ，颠倒字符串中 单词 的顺序。
+     * <p>
+     * 单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+     * <p>
+     * 返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
+     * <p>
+     * 注意：输入字符串 s中可能会存在前导空格、尾随空格或者单词间的多个空格。返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+     * <p>
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：s = "the sky is blue"
+     * 输出："blue is sky the"
+     * 示例 2：
+     * <p>
+     * 输入：s = "  hello world  "
+     * 输出："world hello"
+     * 解释：颠倒后的字符串中不能存在前导空格和尾随空格。
+     * 示例 3：
+     * <p>
+     * 输入：s = "a good   example"
+     * 输出："example good a"
+     * 解释：如果两个单词间有多余的空格，颠倒后的字符串需要将单词间的空格减少到仅有一个。
+     *
+     * @param s
+     * @return
+     */
+    public String reverseWords(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int left = s.length() - 1, right = left + 1;
+        while (left >= 0) {
+            if (s.charAt(left) == ' ') {
+                if (left + 1 != right) {
+                    stringBuilder.append(s, left + 1, right);
+                    if (stringBuilder.length() != 0) {
+                        stringBuilder.append(" ");
+                    }
+                }
+                right = left;
+            }
+            left--;
+        }
+        stringBuilder.append(s, left + 1, right);
+        // 去除后面空格
+        right = stringBuilder.length();
+        for (int i = stringBuilder.length() - 1; i >= 0; i--) {
+            if (stringBuilder.charAt(i) != ' ') {
+                break;
+            }
+            right--;
+        }
+        return stringBuilder.substring(0, right);
+    }
+
+    public String reverseWords2(String s) {
+        int i = 0, right = s.length();
+        // 去除前面空格
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(i) != ' ') {
+                break;
+            }
+            i++;
+        }
+        // 去除后面空格
+        for (int j = s.length() - 1; j >= 0; j--) {
+            if (s.charAt(i) != ' ') {
+                break;
+            }
+            right--;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        int left = right - 1;
+        while (left >= i) {
+            if (s.charAt(left) == ' ') {
+                if (left + 1 != right) {
+                    stringBuilder.append(s, left + 1, right);
+                    stringBuilder.append(" ");
+                }
+                right = left;
+            }
+            left--;
+        }
+        stringBuilder.append(s, left + 1, right);
+        return stringBuilder.toString();
+    }
+
+    public String reverseWords1(String s) {
+        //源字符数组
+        char[] initialArr = s.toCharArray();
+        //新字符数组
+        char[] newArr = new char[initialArr.length + 1];//下面循环添加"单词 "，最终末尾的空格不会返回
+        int newArrPos = 0;
+        //i来进行整体对源字符数组从后往前遍历
+        int i = initialArr.length - 1;
+        while (i >= 0) {
+            while (i >= 0 && initialArr[i] == ' ') {
+                i--;
+            }  //跳过空格
+            //此时i位置是边界或!=空格，先记录当前索引，之后的while用来确定单词的首字母的位置
+            int right = i;
+            while (i >= 0 && initialArr[i] != ' ') {
+                i--;
+            }
+            //指定区间单词取出(由于i为首字母的前一位，所以这里+1,)，取出的每组末尾都带有一个空格
+            for (int j = i + 1; j <= right; j++) {
+                newArr[newArrPos++] = initialArr[j];
+                if (j == right) {
+                    newArr[newArrPos++] = ' ';//空格
+                }
+            }
+        }
+        //若是原始字符串没有单词，直接返回空字符串；若是有单词，返回0-末尾空格索引前范围的字符数组(转成String返回)
+        if (newArrPos == 0) {
+            return "";
+        } else {
+            return new String(newArr, 0, newArrPos - 1);
         }
     }
 }
