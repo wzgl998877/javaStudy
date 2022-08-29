4067d25df9fee83acba21f97f50da3d7701b728b
zhengtao <2458425315@qq.com>
2022-08-30 00:11:49 +0800

    feat(): 简单题

--- src/main/java/com/zt/javastudy/leetcode/LeetCodeForStack.java
+++ src/main/java/com/zt/javastudy/leetcode/LeetCodeForStack.java
@@ -1,9 +1,6 @@
 package com.zt.javastudy.leetcode;
 
-import java.util.Date;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @author zhengtao
@@ -27,6 +24,12 @@ public class LeetCodeForStack {
         LeetCodeForStack leetCodeForStack = new LeetCodeForStack();
         System.out.println(leetCodeForStack.calculate("1+2*2*2+2"));
         System.out.println(leetCodeForStack.calculate2("1-(-2)"));
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1);
+        myQueue.push(2);
+        myQueue.push(3);
+        myQueue.push(4);
+
     }
 
     /**
@@ -406,5 +409,172 @@ public class LeetCodeForStack {
             }
         }
         num.push(temp);
+    }
+
+    /**
+     * 20. 有效的括号
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     * <p>
+     * 有效字符串需满足：
+     * <p>
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * <p>
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：s = "()"
+     * 输出：true
+     * 示例 2：
+     * <p>
+     * 输入：s = "()[]{}"
+     * 输出：true
+     * 示例 3：
+     * <p>
+     * 输入：s = "(]"
+     * 输出：false
+     * 示例 4：
+     * <p>
+     * 输入：s = "([)]"
+     * 输出：false
+     * 示例 5：
+     * <p>
+     * 输入：s = "{[]}"
+     * 输出：true
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.peek() != '{') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    /**
+     * 请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+     * <p>
+     * 实现 MyStack 类：
+     * <p>
+     * void push(int x) 将元素 x 压入栈顶。
+     * int pop() 移除并返回栈顶元素。
+     * int top() 返回栈顶元素。
+     * boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+     *  
+     * <p>
+     * 注意：
+     * <p>
+     * 你只能使用队列的基本操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。
+     * 你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+     */
+    class MyStack {
+        private Queue<Integer> stack;
+
+        public MyStack() {
+            stack = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            if (stack.isEmpty()) {
+                stack.offer(x);
+            } else {
+                Queue<Integer> temp = new LinkedList<>();
+                while (!stack.isEmpty()) {
+                    temp.offer(stack.poll());
+                }
+                stack.offer(x);
+                while (!temp.isEmpty()) {
+                    stack.offer(temp.poll());
+                }
+            }
+        }
+
+        public void push1(int x) {
+            int n = stack.size();
+            stack.offer(x);
+            for(int i = 0; i < n; i++) {
+                stack.offer(stack.poll());
+            }
+        }
+
+        public int pop() {
+            return stack.poll();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public boolean empty() {
+            return stack.isEmpty();
+        }
+    }
+
+    /**
+     * 232. 用栈实现队列
+     * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+     *
+     * 实现 MyQueue 类：
+     *
+     * void push(int x) 将元素 x 推到队列的末尾
+     * int pop() 从队列的开头移除并返回元素
+     * int peek() 返回队列开头的元素
+     * boolean empty() 如果队列为空，返回 true ；否则，返回 false
+     * 说明：
+     *
+     * 你 只能 使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+     * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+     */
+    static class MyQueue {
+        private Deque<Integer> queue;
+        public MyQueue() {
+            queue = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            Deque<Integer> temp = new LinkedList<>();
+            while(!queue.isEmpty()) {
+                temp.push(queue.pop());
+            }
+            temp.push(x);
+            while(!temp.isEmpty()) {
+                queue.push(temp.pop());
+            }
+        }
+
+        public int pop() {
+            return queue.pop();
+        }
+
+        public int peek() {
+            return queue.peek();
+        }
+
+        public boolean empty() {
+            return queue.isEmpty();
+        }
     }
 }
